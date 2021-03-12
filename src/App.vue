7c7f1fbdd6e95fc1718c91aa96e760e8a3e07357
Julian Grisales <jgrisales@gmail.com>
2021-03-12 17:58:43 +0100

    Auth orGuesst zone

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-  <router-view/>
+  <template v-if="user">
+    <router-view/>
+  </template>
+  <div v-if="!user && user !== undefined">
+    <p>Login, register</p>
+  </div>
 </template>
 <script>
+import { onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+import {auth} from './utils/firebase'
+
 export default {
   name: "App",
+
   setup(props) {
-    
+
+    const store = useStore();
+    const user = computed(() => store.state.user)
+
+    onMounted(() => {
+      auth.onAuthStateChanged((user) => {
+        store.commit("setUser", user);
+      })
+    })
+
+    return {
+      user
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: undefined,
   },
+
   mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    }
   },
+
   actions: {
   },
+
   modules: {
   }
 })
