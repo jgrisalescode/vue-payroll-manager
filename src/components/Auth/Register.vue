00d0a9875cc3baf60624f94a522df8c1daf6eda4
Julian Grisales <jgrisales@gmail.com>
2021-03-12 21:55:54 +0100

    Validating form register

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -3,13 +3,28 @@
     <h1>Create account</h1>
     <form class="ui form" @submit.prevent="onRegister()">
       <div class="field">
-        <input type="text" placeholder="Email" v-model="formData.email">
+        <input
+          type="text"
+          placeholder="Email"
+          v-model="formData.email"
+          :class="{ error: formError.email }"
+        />
       </div>
       <div class="field">
-        <input type="password" placeholder="Password" v-model="formData.password">
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="formData.password"
+          :class="{ error: formError.password }"
+        />
       </div>
       <div class="field">
-        <input type="password" placeholder="Repeat password" v-model="formData.repeatPassword">
+        <input
+          type="password"
+          placeholder="Repeat password"
+          v-model="formData.repeatPassword"
+          :class="{ error: formError.repeatPassword }"
+        />
       </div>
       <button type="submit" class="ui button positive fluid">Register</button>
     </form>
@@ -18,56 +33,78 @@
 </template>
 
 <script>
-  export default {
-    name: 'Register',
+import { ref } from "vue";
+import * as Yup from "yup";
 
-    props: {
-      changeForm: Function
-    },
+export default {
+  name: "Register",
 
-    setup(props) {
-      let formData = {}
+  props: {
+    changeForm: Function,
+  },
 
-      const onRegister = () => {
-        console.log(formData)
+  setup(props) {
+    let formData = {};
+    let formError = ref({});
+
+    const schemaForm = Yup.object().shape({
+      email: Yup.string().email(true).required(true),
+      password: Yup.string().required(true),
+      repeatPassword: Yup.string()
+        .required(true)
+        .oneOf([Yup.ref("password")], true),
+    });
+
+    const onRegister = async () => {
+      formError.value = {};
+
+      try {
+        await schemaForm.validate(formData, { abortEarly: false });
+        console.log("Alles Gut");
+      } catch (err) {
+        err.inner.forEach((error) => {
+          formError.value[error.path] = error.message;
+        });
       }
+    };
 
-      return {
-        formData,
-        onRegister
-      }
-    }
-  }
+    return {
+      formData,
+      formError,
+      onRegister,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .register {
-    background-color: white;
-    padding: 30px;
-    box-shadow: 0px 0px 38px -5px rgba(0,0,0,0.45);
-    width: 400px;
-    border-radius: 10px;
+.register {
+  background-color: white;
+  padding: 30px;
+  box-shadow: 0px 0px 38px -5px rgba(0, 0, 0, 0.45);
+  width: 400px;
+  border-radius: 10px;
 
-    h1 {
-      text-align: center;
-      margin-bottom: 30px;
-    }
+  h1 {
+    text-align: center;
+    margin-bottom: 30px;
+  }
 
-    form {
-      input.error {
-        border-color: #faa;
-        background-color: #ffeded;
-      }
-    }
-
-    p {
-      text-align: center;
-      margin-top: 30px;
-
-      &:hover {
-        cursor: pointer;
-        opacity: 0.6;
-      }
+  form {
+    input.error {
+      border-color: #faa;
+      background-color: #ffeded;
     }
   }
+
+  p {
+    text-align: center;
+    margin-top: 30px;
+
+    &:hover {
+      cursor: pointer;
+      opacity: 0.6;
+    }
+  }
+}
 </style>
