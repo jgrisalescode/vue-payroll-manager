a57218e4668e38a33876d67ca6c5f8bb357c824b
Julian Grisales <jgrisales@gmail.com>
2021-03-14 17:22:54 +0100

    Login form styles

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,20 +1,57 @@
 <template>
-  <div>
-    <h2>Login</h2>
-    <button @click="changeForm">Create new account</button>
+  <div class="login">
+    <h1>Login</h1>
+    <form class="ui form">
+      <div class="field">
+        <input type="text" placeholder="Email" />
+      </div>
+      <div class="field">
+        <input type="password" placeholder="Password" />
+      </div>
+      <button type="submit" class="ui button positive fluid">Sign in</button>
+    </form>
+    <p @click="changeForm()">Create new account</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Login",
+export default {
+  name: "Login",
 
-    props: {
-      changeForm: Function
-    }
-  }
+  props: {
+    changeForm: Function,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.login {
+  background-color: white;
+  padding: 30px;
+  box-shadow: 0px 0px 38px -5px rgba($color: #000000, $alpha: 0.45);
+  width: 400px;
+  border-radius: 10px;
 
+  h1 {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  form {
+    input.error {
+      border-color: #faa;
+      background-color: #ffeded;
+    }
+  }
+
+  p {
+    text-align: center;
+    margin-top: 30px;
+
+    &:hover {
+      cursor: pointer;
+      opacity: 0.6;
+    }
+  }
+}
 </style>
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,50 +1,50 @@
 <template>
   <div class="auth">
-    <Login v-if="showLogin" :changeForm="changeForm"/>
-    <Register v-if="!showLogin" :changeForm="changeForm"/>
+    <Login v-if="showLogin" :changeForm="changeForm" />
+    <Register v-if="!showLogin" :changeForm="changeForm" />
   </div>
 </template>
 
 <script>
-  import { ref } from 'vue'
-  import Login from '../components/Auth/Login'
-  import Register from '../components/Auth/Register'
+import { ref } from "vue";
+import Login from "../components/Auth/Login";
+import Register from "../components/Auth/Register";
 
-  export default {
-    name: "Auth",
+export default {
+  name: "Auth",
 
-    components: {
-      Login,
-      Register
-    },
+  components: {
+    Login,
+    Register,
+  },
 
-    setup(props) {
-      let showLogin = ref(false)
+  setup(props) {
+    let showLogin = ref(true);
 
-      const changeForm = () => {
-        showLogin.value = !showLogin.value
-      }
-      
-      return {
-        showLogin,
-        changeForm
-      }
-    }
-  }
+    const changeForm = () => {
+      showLogin.value = !showLogin.value;
+    };
+
+    return {
+      showLogin,
+      changeForm,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .auth {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background: rgb(4, 78, 177);
-    background: linear-gradient(
-      0deg,
-      rbga(4,78,157, 1) 0%,
-      rgba(0,174,255,1) 100%
-    )
-  }
+.auth {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: rgb(4, 78, 177);
+  background: linear-gradient(
+    0deg,
+    rbga(4, 78, 157, 1) 0%,
+    rgba(0, 174, 255, 1) 100%
+  );
+}
 </style>
