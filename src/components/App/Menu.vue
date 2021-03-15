15506fd2dccfc2a3937959252d3a6d73ce8e099f
Julian Grisales <jgrisales@gmail.com>
2021-03-15 15:14:17 +0100

    Menu styles

---
+++ src/components/App/Menu.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="ui secondary menu">
+    <div class="ui container">
+      <div class="menu left">
+        <router-link class="item" to="/">Payrolls</router-link>
+      </div>
+      <router-link class="menu center" to="/">
+        <img
+          src="../../assets/nomina.png"
+          alt="Logo Payrolls"
+          class="ui small image"
+        />
+      </router-link>
+      <div class="menu right">
+        <router-link class="item" to="/account">Hello, User</router-link>
+        <span class="ui item logout">
+          <i class="sign-out icon"></i>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+};
+</script>
+
+<style lang="scss" scoped>
+.ui.menu.secondary {
+  background-color: #16202b;
+
+  .item {
+    color: white;
+
+    &:hover {
+      color: #1fa1f1;
+    }
+  }
+
+  .menu.left {
+    width: 50%;
+  }
+
+  .menu.right {
+    width: 50%;
+    justify-content: flex-end;
+
+    .logout:hover {
+      cursor: pointer;
+      i {
+        color: red;
+      }
+    }
+  }
+
+  .menu.center {
+    position: relative;
+    background-color: #16212b;
+    top: 15px;
+    padding: 20px;
+    margin: -20px;
+    border-radius: 50%;
+
+    &:hover {
+      box-shadow: 0px 10px 10px -3px rgba($color: #000000, $alpha: 0.3);
+      -webkit-box-shadow: 0px 10px 10px -3px rgba($color: #000000, $alpha: 0.3);
+      -moz-box-shadow: 0px 10px 10px -3px rgba($color: #000000, $alpha: 0.3);
+    }
+
+    .ui.image {
+      width: 40px;
+    }
+  }
+}
+</style>
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="basic-layout">
-    <h2>Menu</h2>
+    <Menu />
     <div class="ui container">
       <slot />
     </div>
@@ -8,7 +8,14 @@
 </template>
 
 <script>
-export default {};
+import Menu from "../components/App/Menu";
+
+export default {
+  name: "BasicLayout",
+  components: {
+    Menu,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
