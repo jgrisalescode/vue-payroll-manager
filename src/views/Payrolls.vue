455afc782f9cf265e209d934333702760c7e683a
Julian Grisales <jgrisales@gmail.com>
2021-03-15 14:49:26 +0100

    Layout

---
+++ src/views/Payrolls.vue
@@ -0,0 +1,23 @@
+<template>
+  <BasicLayout>
+    <div>
+      <h1>We are in Payroll</h1>
+      <p>Hello World</p>
+      <button>Logout</button>
+    </div>
+  </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from "../layouts/BasicLayout";
+
+export default {
+  name: "Payrolls",
+  components: {
+    BasicLayout,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
