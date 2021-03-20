9a6462c7259f8fb5cb4f2da04025d62a42fa288e
Julian Grisales <jgrisales@gmail.com>
2021-03-20 14:17:23 +0100

    Account form logic

--- src/components/Account/ChangeName.vue
+++ src/components/Account/ChangeName.vue
@@ -1,15 +1,54 @@
 <template>
   <div>
-    <form class="ui form change-name">
-      <input type="text" placeholder="Fullname" />
+    <form class="ui form change-name" @submit.prevent="onChangeName">
+      <input
+        type="text"
+        placeholder="Fullname"
+        v-model="name"
+        :class="{ error: formError }"
+      />
       <button type="submit" class="ui button primary">Update</button>
     </form>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import * as Yup from "yup";
+
 export default {
   name: "ChangeName",
+
+  setup(props) {
+    let name = ref("");
+    let formError = ref(false);
+
+    const schemaForm = Yup.object().shape({
+      name: Yup.string().min(6, true).required(true),
+    });
+
+    const onChangeName = async () => {
+      formError.value = false;
+      console.log("Changing name...");
+      console.log("New name: " + name.value);
+
+      try {
+        await schemaForm.validate({ name: name.value }, { abortEarly: false });
+        console.log("Data OK!!!✔");
+      } catch (err) {
+        console.log("Data ERROR !!!❌");
+        err.inner.forEach((error) => {
+          formError.value = error.message;
+        });
+      }
+    };
+
+    return {
+      name,
+      onChangeName,
+      formError,
+    };
+  },
 };
 </script>
 
