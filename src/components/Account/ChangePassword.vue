c905071b4274cc9e4a2fe46a9374d7dc1284cded
Julian Grisales <jgrisales@gmail.com>
2021-03-21 11:22:54 +0100

    Recovery data from change password form

--- src/components/Account/ChangePassword.vue
+++ src/components/Account/ChangePassword.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-    <form class="ui form change-password">
+    <form class="ui form change-password" @submit.prevent="onChangePassword">
       <div class="three fields">
         <div class="field">
-          <input type="password" placeholder="Current password" />
+          <input
+            type="password"
+            placeholder="Current password"
+            v-model="formData.currentPassword"
+            :class="{ error: formError.currentPassword }"
+          />
         </div>
         <div class="field">
-          <input type="password" placeholder="New password" />
+          <input
+            type="password"
+            placeholder="New password"
+            v-model="formData.newPassword"
+            :class="{ error: formError.newPassword }"
+          />
         </div>
         <div class="field">
-          <input type="password" placeholder="Repeat new password" />
+          <input
+            type="password"
+            placeholder="Repeat new password"
+            v-model="formData.repeatNewPassword"
+            :class="{ error: formError.repeatNewPassword }"
+          />
         </div>
       </div>
       <button class="ui button primary" type="submit">Update</button>
@@ -18,8 +33,42 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import * as Yup from "yup";
+
 export default {
   name: "ChangePassword",
+
+  setup(props) {
+    let formData = {};
+    let formError = ref({});
+
+    const schemaForm = Yup.object().shape({
+      currentPassword: Yup.string().required(true),
+      newPassword: Yup.string().required(true),
+      repeatNewPassword: Yup.string()
+        .required(true)
+        .oneOf([Yup.ref("newPassword")], true),
+    });
+
+    const onChangePassword = async () => {
+      formError.value = {};
+      try {
+        await schemaForm.validate(formData, { abortEarly: false });
+        console.log("Alles gut");
+      } catch (err) {
+        err.inner.forEach((error) => {
+          formError.value[error.path] = error.message;
+        });
+      }
+    };
+
+    return {
+      formData,
+      formError,
+      onChangePassword,
+    };
+  },
 };
 </script>
 
