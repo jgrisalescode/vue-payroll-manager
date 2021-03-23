fef687db804a9608b3f265ab540a76a55dac215c
Julian Grisales <jgrisales@gmail.com>
2021-03-23 13:03:09 +0100

    Recovery data of payroll form

--- src/components/Payrolls/UploadPayroll.vue
+++ src/components/Payrolls/UploadPayroll.vue
@@ -6,7 +6,7 @@
     <form
       class="ui form upload-payroll__form"
       :class="{ open: showForm }"
-      @submit.prevent=""
+      @submit.prevent="handleSubmit"
     >
       <div class="field">
         <!-- Custom a input file using a label id matches 😅-->
@@ -14,12 +14,17 @@
           <i class="file icon"></i>
           Select payroll
         </label>
-        <input type="file" id="file" style="display: none" />
+        <input
+          type="file"
+          id="file"
+          style="display: none"
+          @change="uploadFile"
+        />
       </div>
       <div class="field">
         <div class="ui calendar">
           <div class="ui input left icon">
-            <input type="date" />
+            <input type="date" @change="changeDate" :value="date" />
           </div>
         </div>
       </div>
@@ -36,14 +41,42 @@ export default {
 
   setup(props) {
     let showForm = ref(false);
+    let file = ref(null);
+    let date = ref(null);
+    let loading = ref(false);
+    let error = ref(null);
 
     const showCloseForm = () => {
       showForm.value = !showForm.value;
     };
 
+    const uploadFile = (e) => {
+      const fileTemp = e.target.files[0];
+      if (fileTemp.type === "application/pdf") {
+        file.value = fileTemp;
+      }
+    };
+
+    const changeDate = (e) => {
+      date.value = e.target.value;
+    };
+
+    const handleSubmit = () => {
+      console.log("Uploading payroll");
+      console.log("File -> ", file.value);
+      console.log("Date ->", date.value);
+    };
+
     return {
       showForm,
       showCloseForm,
+      uploadFile,
+      changeDate,
+      handleSubmit,
+      file,
+      date,
+      loading,
+      error,
     };
   },
 };
